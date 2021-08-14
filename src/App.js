2af47fdf2292d8b9fb1ac69a35a6f06b8767acee
umair <umair@umairs-MacBook-Air.local>
2021-08-13 23:44:02 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import React,{useState} from "react";
+
 
 function App() {
+
+  const api = {
+    key:"5f655f096937812ed818b883bf33b173",
+    base:"https://api.openweathermap.org/data/2.5/"
+
+}
+  const [query,setQuery] = useState('')
+  const [weather,setWeather] = useState({})
+
+
+  const search = (evt) => {
+
+    if(evt.key === "Enter")
+    {
+      console.log(api.base + 'abc')
+
+        fetch(api.base + 'weather?q=' + query + '&units=metric&APPID=' + api.key)
+        .then(res=> res.json())
+        .then(result =>
+          {
+          console.log(result)
+          setQuery('');
+          setWeather(result)
+          })
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text" 
+            className="search-bar" 
+            placeholder="search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            ></input>
+        </div>
+        {typeof weather.main != "undefined" ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name} , {weather.sys.country}</div>
+            <div className="date">{new Date().toDateString()}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">{Math.round(weather.main.temp)} c</div>
+            <div className="weather">{weather.weather[0].description}</div>
+          </div>
+        </div>) : ('')}
+      </main>
+
     </div>
   );
 }
